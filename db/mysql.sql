0578c3c913da0b9aa0d1b099a07ec3dff55ed7ef
Dao Ngoc <ngoc@Daos-MacBook-Pro.local>
2013-10-30 23:28:40 +0900

    Note about creating index: 1071 - Specified key was too long; max key length is 767 bytes http://stackoverflow.com/questions/1814532/1071-specified-key-was-too-long-max-key-length-is-767-bytes

--- db/mysql.sql
+++ db/mysql.sql
@@ -1,8 +1,27 @@
 DROP SCHEMA IF EXISTS re_svc_user_mgt;
-CREATE SCHEMA IF NOT EXISTS re_svc_user_mgt DEFAULT CHARACTER SET utf8;
+CREATE SCHEMA IF NOT EXISTS re_svc_user_mgt DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;
 USE re_svc_user_mgt;
 
+/*
+Note about creating index:
+#1071 - Specified key was too long; max key length is 767 bytes
+http://stackoverflow.com/questions/1814532/1071-specified-key-was-too-long-max-key-length-is-767-bytes
+*/
+
 /* Core tables ---------------------------------------------------------------*/
+
+CREATE TABLE admins(
+  id INT NOT NULL AUTO_INCREMENT,
+  created_at TIMESTAMP NOT NULL DEFAULT 0,
+  updated_at TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+
+  username VARCHAR(1024) NOT NULL,  /* A URI, email, or text username */
+  auth_type INT NOT NULL,           /* A short integer representing the type of authorization system */
+  password VARCHAR(256) NOT NULL,
+
+  PRIMARY KEY (id),
+  KEY index_username (username(255))
+) ENGINE=InnoDB;
 
 CREATE TABLE clients(
   id INT NOT NULL AUTO_INCREMENT,
@@ -12,7 +31,7 @@ CREATE TABLE clients(
   name VARCHAR(1024) NOT NULL,
 
   PRIMARY KEY (id)
-) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB;
 
 CREATE TABLE users(
   id INT NOT NULL AUTO_INCREMENT,
@@ -22,7 +41,7 @@ CREATE TABLE users(
   enabled TINYINT NOT NULL DEFAULT 0,
 
   PRIMARY KEY (id)
-) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB;
 
 CREATE TABLE credentials(
   id INT NOT NULL AUTO_INCREMENT,
@@ -38,7 +57,7 @@ CREATE TABLE credentials(
 
   PRIMARY KEY (id),
   KEY index_user_id (user_id)
-) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB;
 
 /* Log tables ----------------------------------------------------------------*/
 
@@ -48,7 +67,6 @@ CREATE TABLE credentials(
 
 INSERT INTO clients(created_at, name) VALUES (NOW(), 'opclient1');
 
-INSERT INTO users(created_at, enabled) VALUES (NOW(), 1);
-
-INSERT INTO credentials(created_at, user_id, username, auth_type, password, validated)
- VALUES (NOW(), 1, 'opadmin', 999, 'xxx', 1);
+/* Password: test123! (TODO: hash the password */
+INSERT INTO admins(created_at, username, auth_type, password)
+ VALUES (NOW(), 'opadmin', 999, 'test123!');
