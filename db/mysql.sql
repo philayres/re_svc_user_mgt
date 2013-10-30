cd6fd9a1c391adb90e8591c6d3147768b75cce34
Dao Ngoc <ngoc@Daos-MacBook-Pro.local>
2013-10-31 00:20:13 +0900

    Remove table admins

--- db/mysql.sql
+++ db/mysql.sql
@@ -15,19 +15,6 @@ auth_type: A short integer representing the type of authorization system
 
 /* Core tables ---------------------------------------------------------------*/
 
-CREATE TABLE admins(
-  id INT NOT NULL AUTO_INCREMENT,
-  created_at TIMESTAMP NOT NULL DEFAULT 0,
-  updated_at TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-
-  username VARCHAR(1024) NOT NULL,
-  auth_type INT NOT NULL,
-  password VARCHAR(256) NOT NULL,
-
-  PRIMARY KEY (id),
-  KEY index_username (username(255))
-) ENGINE=InnoDB;
-
 CREATE TABLE clients(
   id INT NOT NULL AUTO_INCREMENT,
   created_at TIMESTAMP NOT NULL DEFAULT 0,
@@ -35,6 +22,7 @@ CREATE TABLE clients(
 
   name VARCHAR(1024) NOT NULL,
   client_type INT NOT NULL,
+  shared_secret VARCHAR(255) NOT NULL,
 
   PRIMARY KEY (id)
 ) ENGINE=InnoDB;
@@ -44,6 +32,7 @@ CREATE TABLE users(
   created_at TIMESTAMP NOT NULL DEFAULT 0,
   updated_at TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
 
+  is_admin TINYINT NOT NULL DEFAULT 0,
   enabled TINYINT NOT NULL DEFAULT 0,
 
   PRIMARY KEY (id)
@@ -58,7 +47,8 @@ CREATE TABLE credentials(
 
   username VARCHAR(1024) NOT NULL,
   auth_type INT NOT NULL,
-  password VARCHAR(256) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  salt VARCHAR(255) NOT NULL,
   validated TINYINT NOT NULL DEFAULT 0,
 
   PRIMARY KEY (id),
@@ -72,8 +62,12 @@ CREATE TABLE credentials(
 
 /* Bootstrap data ------------------------------------------------------------*/
 
-INSERT INTO clients(created_at, name, client_type) VALUES (NOW(), 'opclient1', 999);
+INSERT INTO clients(created_at, name, client_type, shared_secret)
+ VALUES (NOW(), 'opclient1', 999, 'test123!');
+
+INSERT INTO users(created_at, is_admin, enabled)
+ VALUES (NOW(), 1, 1);
 
 /* Password: test123! (TODO: hash the password) */
-INSERT INTO admins(created_at, username, auth_type, password)
- VALUES (NOW(), 'opadmin', 999, 'test123!');
+INSERT INTO credentials(created_at, user_id, username, auth_type, password, salt, validated)
+ VALUES (NOW(), 1, 'opadmin', 999, 'test123!', 'salt', 1);
